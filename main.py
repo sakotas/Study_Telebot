9c699cee73833a2fe44cb0a0f7ab62883661526e
AAKotishevskii <sakotas@users.noreply.github.com>
2024-08-19 18:39:05 +0300

    Add basic framework to bot

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello World')
+import telebot
+
+bot = telebot.TeleBot('7483627073:AAE0PN-TscROJLj7dqYRDOct_5b3wKBeW6g')
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+
+
+bot.polling(none_stop=True)
